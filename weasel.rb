d649a0299129f939834c97324e327522a04ad093
SovietKetchup <sam.comer+git@gmail.com>
2016-03-01 17:37:16 +0000

    Fixed syntax of accessing 2D arrays

--- weasel.rb
+++ weasel.rb
@@ -8,30 +8,34 @@ class Weasel
   attr_reader :goal
   def initialize goal
     @word = [ [], [] ]
-    @word[0, 0] = goal.upcase
-    @word[0, 1] = generate_str
+    @word[0][0] = goal.upcase
+    @word[0][1] = generate_str
     run
   end
   
   private
   # Run the logic
   def run
+  
+    puts @word[0][0]
+  
     # Keep running until a succesful copy has been made
-    until @word[0, 1] == @word[0, 0]
+    until @word[0][1] == @word[0][0]
       # Generate 100 mutated copies with each char having a 5% chance to mutate
-      @word = copy
+      copy
       
       # Take best scoring copy
-      @word[0, 1] = @word[ 1, rate.each_with_index.max[1] ]
+      @word[0][1] = @word[1][@word[1].each_with_index.max[1]]
       
       # loopy loop
+      puts "#{@word[0][1]} : #{}"
     end
   end
   
   # Create 100 mutated copies
   def copy
     100.times do |c|
-      @word[1, c] = mutate(@word[0, 1])
+      @word[1][c] = mutate(@word[0][1])
     end
   end
   
@@ -39,8 +43,10 @@ class Weasel
   def rate
     score = []
     @word[1].size.times do |c|
-      score[c] = compare (@word[1, c])
+      score[c] = compare (@word[1][c])
+      c += 1
     end
+    score
   end
   
   # Mutate each character of the string on a 5% (5/100) chance
@@ -50,15 +56,16 @@ class Weasel
   
   # Get rating of string against the goal
   def compare str
-    @word[0, 0].each_char.to_a.zip(str.each_char.to_a).map{|n| n[0]==n[1] ? 1 : 0}.inject{ |i,j| i+j } 
+    @word[0][0].scan(/./).zip(str.each).map{|n| n[0]==n[1] ? 1 : 0}.inject{ |i,j| i+j } 
   end
   
   # Generate a string of the original's length
   def generate_str
     str = ""
-    @word[0, 0].length.times do 
-      str += LETTERS.sample
+    @word[0][0].length.times do 
+      str +=  LETTERS.sample
     end
+    str
   end
   
   # Generate a letter DIFFERENT to the current letter
