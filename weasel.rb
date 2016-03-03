c92f6e175db672c4056ad1e463cfb3c8db956a7b
SovietKetchup <sam.comer+git@gmail.com>
2016-03-03 20:17:35 +0000

    Removed dependencies of instance variable within methods (for testing). Added setup ready for testing

--- weasel.rb
+++ weasel.rb
@@ -7,29 +7,30 @@ class Weasel
   LETTERS = ("A".."Z").to_a + [" "]
   attr_reader :args
   def initialize args = {}
-    args = {:goal => "METHINKS IT IS LIKE A WEASEL", :copies => 100, 
+    @args = {:goal => "METHINKS IT IS LIKE A WEASEL", :copies => 100, 
             :chance => 2.5}.merge(args)  # defaults
-    @goal = args[:goal].upcase
-    @copies = args[:copies]
-    @chance = args[:chance].to_f/100
+    @args[:goal] = @args[:goal]
+    @args[:copies] = @args[:copies]
+    @args[:chance] = @args[:chance]
+    
   end
   
   # Run the logic
   def run
-    @current = generate_str
+    current = generate_str
     c = 0
     # Keep running until a succesful copy has been made
-    until @current == @goal
+    until current == @args[:goal]
       # Generate mutated copies with each char having a % chance to mutate
-      t = copy_mutate
+      t = copy_mutate(current, @args[:copies], @args[:chance])
       
       # Get highest scoring mutation of string
       top_s = 0
       top = ""
-      @current = t.sort_by { |str| compare(str) }.last
+      current = t.sort_by { |str| compare(@args[:goal], str) }.last
       
       # If it's an array, make it a string
-      @current.is_a?(Array) ? @current = @current.join : @current = @current
+      current.is_a?(Array) ? current = current.join : current = current
       
       c += 1
     end
@@ -38,25 +39,25 @@ class Weasel
   end
   
   # Create 100 mutated copies
-  def copy_mutate
+  def copy_mutate current, copies, chance
     mut = []
     # Create 100 mutations of the current string
-    @copies.times do |c|
-      mut[c] = mutate(@current)
+    copies.times do |c|
+      mut[c] = mutate(current, chance)
     end
     mut
   end
   
   # Mutate each character of the string on a 2.5% chance
-  def mutate str
+  def mutate str, chance
     # Convert to a string if it's an array
     str = str.is_a?(Array) ? str.join : str
-    str.each_char.map{|x| rand < @chance ? generate_ltr(x) : x}
+    str.each_char.map{|x| rand < chance ? generate_ltr(x) : x}
   end
   
   # Get rating of string against the goal
-  def compare arr
-    @goal.scan(/./).zip(arr).map{|n| n[0]==n[1] ? 1 : 0}.inject{ |i,j| i+j } 
+  def compare goal, arr
+    goal.scan(/./).zip(arr).map{|n| n[0]==n[1] ? 1 : 0}.inject{ |i,j| i+j } 
   end
   
   # Generate a letter DIFFERENT to the current letter
@@ -68,7 +69,7 @@ class Weasel
   # Generate a string of the original's length
   def generate_str
     s = ""
-    @goal.length.times do 
+    @args[:goal].length.times do 
       s += LETTERS.sample
     end
     s
@@ -77,8 +78,12 @@ end
 
 # Create a new Weasel object with the goal string, number of copies per cycle 
 # and % chance of mutation per character
-w = Weasel.new :goal => "METHINKS IT IS LIKE A WEASEL", :copies => 100, :chance => 2
+w = Weasel.new :goal => "METHINKS IT IS LIKE A WEASEL", :copies => 100, :chance => 0.02
 
 # Returns the number of cycles taken to acheive goal string
+puts w.run
+
+# Change an argument to run again
+w.args[:goal] = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ"
 puts w.run
 
