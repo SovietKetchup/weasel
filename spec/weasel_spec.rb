432598dc6927f84e3f558ef081e2126b7088bab3
SovietKetchup <sam.comer+git@gmail.com>
2016-03-03 21:31:51 +0000

    Added tests for compare method. Fixed generate_str test unecessary value

--- spec/weasel_spec.rb
+++ spec/weasel_spec.rb
@@ -4,7 +4,8 @@ require_relative '../weasel.rb'
 describe Weasel do 
   before(:each) { @weasle = Weasel.new }
   
-  describe "generate a letter different to the current letter" do 
+  # generate_ltr method
+  describe "generate a letter different to the current letter ::" do 
     before(:each) do 
       @w = Weasel.new
     end
@@ -18,14 +19,34 @@ describe Weasel do
     end
   end
   
-  describe "generate a string of a given length" do
+  # generate_str method
+  describe "generate a string of a given length ::" do
     before(:each) do
-      @w = Weasel.new
-      @w.instance_variable_set("@args", {:goal => "0123456789"})
+      @w = Weasel.new :goal => "0123456789"
     end
     
     it "should be the length of @args[:goal]" do
       @w.generate_str.length.should be 10
+    end
+  end
+  
+  # compare method
+  describe "give a score based on similarity ::" do
+    before(:each) do
+      @w = Weasel.new
+      @goal = "ABCDEFGHIJ"
+    end 
+    
+    it "should be an integer" do
+      @w.compare(@goal, ["A","J","G","H","S","U","E","L","F","P"]).should be_a(Integer)
+    end
+    
+    it "should be out of :goal's length" do
+      @w.compare(@goal, ["A","J","G","H","S","U","E","L","F","P"]).should be <= @goal.length
+    end
+    
+    it "should be 3" do
+      @w.compare(@goal, ["A","J","G","D","S","U","G","L","F","P"]).should be 3
     end
   end
 end
