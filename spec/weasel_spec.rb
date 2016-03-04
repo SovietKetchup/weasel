f90fd2ea339741b60a5e65f5202daab88f3efda8
SovietKetchup <sam.comer+git@gmail.com>
2016-03-04 15:56:04 +0000

    Organised tests

--- spec/weasel_spec.rb
+++ spec/weasel_spec.rb
@@ -7,27 +7,37 @@ describe Weasel do
   # generate_ltr method
   describe "generate a letter different to the current letter ::" do 
     before(:each) do 
-      @w = Weasel.new
+      @ltr = Weasel.new.generate_ltr("A")
+    end
+    
+    it "should be a string" do
+      @ltr.should be_a String
     end
     
     it "should return a letter different to current" do
-      @w.generate_ltr("A").should_not be "A"
+      @ltr.should_not be "A"
     end
   
     it "should be one character" do
-      @w.generate_ltr("A").length.should be 1
+      @ltr.length.should be 1
     end
   end
   
   # generate_str method
   describe "generate a string of a given length ::" do
     before(:each) do
-      @w = Weasel.new :goal => "0123456789"
+      @str = Weasel.new(:goal => "0123456789").generate_str
     end
     
-    it "should be the length of @args[:goal]" do
-      @w.generate_str.length.should be 10
+    it "should be a string" do
+      @str.should be_a String
     end
+    
+    it "should be the length of the goal string" do
+      @str.length.should be 10
+    end
+    
+
   end
   
   # compare method
@@ -38,7 +48,7 @@ describe Weasel do
     end 
     
     it "should be an integer" do
-      @w.compare(@goal, ["A","J","G","H","S","U","E","L","F","P"]).should be_a(Integer)
+      @w.compare(@goal, ["A","J","G","H","S","U","E","L","F","P"]).should be_a Integer
     end
     
     it "should be out of :goal's length" do
